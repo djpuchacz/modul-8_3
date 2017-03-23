274ad7d5b949af16f80b5ca5cf1b96b6891699e1
Łukasz Woźniak <ukasz Woniak>
2017-03-23 17:02:13 +0100

    dodanie plików

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+/* 
+jakiś tam komentarz blokowy nr 1
+*/
+var name = prompt('Wpisz swoje imię');
+// jakiś tam komentarz liniowy
+alert('Witaj, ' + name);
+/* 
+jakiś tam komentarz blokowy nr 2
+console.log('Witaj, ' + name);
+*/
